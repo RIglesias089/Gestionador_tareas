73c52d8f26b50bf591b14b5b29cdc4a24c72f744
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-21 14:11:15 -0600

    Feat: Creacion de la clase Usuario

---
+++ SnowList/src/main/java/org/model/Usuario.java
@@ -0,0 +1,30 @@
+package org.model;
+
+public class Usuario {
+
+    //Primero empezamos planteando los distintos atributos de la clase
+    private String id_usuario;
+    private String nombre_usuario;
+    private String email;
+    private String password;
+    private boolean premium;
+    private String telefono;
+
+    //Para poder usar los atributos de la clase planteamos un constructor
+    //Este mismo inicializa los objetos
+
+    public Usuario(String id_usuario,
+                   String nombre_usuario,
+                   String email,
+                   String password,
+                   boolean premium,
+                   String telefono) {
+        this.id_usuario = id_usuario;
+        this.nombre_usuario = nombre_usuario;
+        this.email = email;
+        this.password = password;
+        this.premium = premium;
+        this.telefono = telefono;
+    }
+
+}
