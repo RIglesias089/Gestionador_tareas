2b85efae8131f0e40330c2f5ee077025b36bd519
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-21 15:06:32 -0600

    Feat: Creacion de la clase Tarea hija de la clase Evento

---
+++ SnowList/src/main/java/org/model/Tarea.java
@@ -0,0 +1,12 @@
+package org.model;
+
+public class Tarea extends Evento{
+    public Tarea(
+            String id_evento,
+            String nombre_evento,
+            String descripcion
+    ) {
+
+        super(id_evento, nombre_evento, descripcion);
+    }
+}
