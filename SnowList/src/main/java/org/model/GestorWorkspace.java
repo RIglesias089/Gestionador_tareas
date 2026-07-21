e315eebd9d24658aa7d708621258412b80c3ffd8
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-21 14:42:10 -0600

    Feat: Creacion de la clase GestorWorkspace

---
+++ SnowList/src/main/java/org/model/GestorWorkspace.java
@@ -0,0 +1,21 @@
+package org.model;
+
+public class GestorWorkspace {
+    //Planteamos sus atributos
+    private String id_gestor;
+    private String nombre_gestor;
+    private String descripcion;
+
+    //Creamos el constructor de el GestoWorkspace
+    public gestorWorkspace(
+            String id_gestor,
+            String nombre_gestor,
+            String descripcion
+    ){
+        this.id_gestor = id_gestor;
+        this.nombre_gestor = nombre_gestor;
+        this.descripcion = descripcion;
+        
+    }
+
+}
