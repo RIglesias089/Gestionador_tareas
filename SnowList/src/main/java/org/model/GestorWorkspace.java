a71632a449fad08d3d105899e4b3e96082ca7385
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-22 21:59:25 -0600

    Feat: Agregado de la lista y sus respectivos datos en el gestor

--- SnowList/src/main/java/org/model/GestorWorkspace.java
+++ SnowList/src/main/java/org/model/GestorWorkspace.java
@@ -1,20 +1,34 @@
 package org.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GestorWorkspace {
-    //Planteamos sus atributos
+
     private String id_gestor;
     private String nombre_gestor;
     private String descripcion;
 
-    //Creamos el constructor de el GestoWorkspace
-    public gestorWorkspace(
+    private List<Usuario> miembros;
+
+    private List<Evento> eventos;
+    private Usuario creador;
+
+    public GestorWorkspace(
             String id_gestor,
             String nombre_gestor,
-            String descripcion
-    ){
+            String descripcion,
+            List<Usuario> miembros,
+            Usuario creador
+    ) {
+
         this.id_gestor = id_gestor;
         this.nombre_gestor = nombre_gestor;
         this.descripcion = descripcion;
+
+        this.miembros = miembros;
+        this.eventos = new ArrayList<>();
+        this.creador = creador;
 
     }
 
