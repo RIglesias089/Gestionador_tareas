26106c54a8e48ff3e0ad6dd3ce9a744f673cad5d
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-22 22:09:30 -0600

    Refactor: Reorganizacion de los metodos en la clase Usuario

--- SnowList/src/main/java/org/model/Usuario.java
+++ SnowList/src/main/java/org/model/Usuario.java
@@ -1,56 +1,52 @@
 package org.model;
 
-/*Importamos la interfaz auteticable y Tambien importamos un list,
- para poder permitir que podamos crear listas dinamicas para la funcionalidad de guardar los usuarios*/
 import org.interfaces.Autenticable;
+
 import java.util.ArrayList;
 import java.util.List;
 
-/*Hacemos que la clase implemente el autenticable Es importante saber que al ser abstracta esta no puede inicializar objetos*/
-public abstract class Usuario implements Autenticable{
+public abstract class Usuario implements Autenticable {
 
-    //Primero empezamos planteando los distintos atributos de la clase
     private String id_usuario;
     private String nombre_usuario;
     private String email;
     private String password;
-    private boolean premium;
+    private boolean premiun;
     private String telefono;
     private int limiteColaboradores;
 
-    //Creamos una lista dinamica para el gestor de Workspace
-    private List<GestorWorkspace> worksapce;
+    private List<GestorWorkspace> workspaces;
 
-    //Para poder usar los atributos de la clase planteamos un constructor
-    //Este mismo inicializa los objetos
-    public Usuario(String id_usuario,
-                   String nombre_usuario,
-                   String email,
-                   String password,
-                   boolean premium,
-                   String telefono) {
+    public Usuario() {}
+
+    public Usuario(
+            String id_usuario,
+            String nombre_usuario,
+            String email,
+            String password,
+            boolean premiun,
+            String telefono
+    ) {
+
         this.id_usuario = id_usuario;
         this.nombre_usuario = nombre_usuario;
         this.email = email;
         this.password = password;
-        this.premium = premium;
+        this.premiun = premiun;
         this.telefono = telefono;
         this.limiteColaboradores = 0;
 
-        this.worksapce = new ArrayList<>();
+        this.workspaces = new ArrayList<>();
     }
 
-    //Creamos el getter o capturador para crear un workspace
-    public List<GestorWorkspace> getWorksapce() {
-        return worksapce;
+    public List<GestorWorkspace> getWorkspaces() {
+        return workspaces;
     }
 
-    //Creamos el setter o lector para ver si hay nuevos workspace
-    public void setWorksapce(List<GestorWorkspace> worksapce) {
-        this.worksapce = worksapce;
+    public void setWorkspaces(List<GestorWorkspace> workspaces) {
+        this.workspaces = workspaces;
     }
 
-    //Creamos los getters y setters para le resto de datos
     public String getId_usuario() {
         return id_usuario;
     }
@@ -82,7 +78,6 @@ public abstract class Usuario implements Autenticable{
     public void settelefono(String telefono) {
         this.telefono = telefono;
     }
-
     public String getPassword() {
         return password;
     }
@@ -92,11 +87,11 @@ public abstract class Usuario implements Autenticable{
     }
 
     public boolean isPremiun() {
-        return premium;
+        return premiun;
     }
 
-    public void setPremiun(boolean premium) {
-        this.premium = premium;
+    public void setPremiun(boolean premiun) {
+        this.premiun = premiun;
     }
 
     public int getLimiteColaboradores() {
@@ -142,5 +137,4 @@ public abstract class Usuario implements Autenticable{
         return this.email.equals(email)
                 && this.password.equals(password);
     }
-
 }
