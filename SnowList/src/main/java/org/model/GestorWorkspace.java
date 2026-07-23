a3e1064ccecb9d69b50b9af369395040794a7687
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-22 22:00:37 -0600

    Feat: Implementacion de los getter y setters respectivos de la clase GestorWorkspace

--- SnowList/src/main/java/org/model/GestorWorkspace.java
+++ SnowList/src/main/java/org/model/GestorWorkspace.java
@@ -32,4 +32,53 @@ public class GestorWorkspace {
 
     }
 
+    //Implementamos los getters y setters respectivos para el gestor de workspace
+    public String getId_gestor() {
+        return id_gestor;
+    }
+
+    public void setId_gestor(String id_gestor) {
+        this.id_gestor = id_gestor;
+    }
+
+    public String getNombre_gestor() {
+        return nombre_gestor;
+    }
+
+    public void setNombre_gestor(String nombre_gestor) {
+        this.nombre_gestor = nombre_gestor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Usuario> getMiembros() {
+        return miembros;
+    }
+
+    public void setMiembros(List<Usuario> miembros) {
+        this.miembros = miembros;
+    }
+
+    public List<Evento> getEventos() {
+        return eventos;
+    }
+
+    public void setEventos(List<Evento> eventos) {
+        this.eventos = eventos;
+    }
+
+    public Usuario getCreador() {
+        return creador;
+    }
+
+    public void setCreador(Usuario creador) {
+        this.creador = creador;
+    }
+
 }
