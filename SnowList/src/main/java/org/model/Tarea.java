af14a9871e8934e6a5113c0cec9bda0c9b45f6d5
RIglesias089 <iglesiasricardozhongli@gmail.com>
2026-07-23 19:20:03 -0600

    Feat: Creacion de atributos en la clase Tarea

--- SnowList/src/main/java/org/model/Tarea.java
+++ SnowList/src/main/java/org/model/Tarea.java
@@ -1,12 +1,35 @@
 package org.model;
 
-public class Tarea extends Evento{
+import org.enums.Estado;
+import org.enums.Prioridad;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Tarea extends Evento {
+
+    private Prioridad prioridad;
+    private Estado estado;
+    private List<ChecklistItem> checklist;
+
     public Tarea(
             String id_evento,
             String nombre_evento,
-            String descripcion
+            String descripcion,
+            Usuario creador,
+            LocalDate fecha,
+            Prioridad prioridad,
+            Estado estado
     ) {
 
-        super(id_evento, nombre_evento, descripcion);
+        super(id_evento, nombre_evento, descripcion, creador, fecha);
+
+        this.tipo = "TAREA";
+        this.prioridad = prioridad;
+        this.estado = estado;
+        this.checklist = new ArrayList<>();
+        this.setColor(obtenerColorPorPrioridad(prioridad));
     }
 }
